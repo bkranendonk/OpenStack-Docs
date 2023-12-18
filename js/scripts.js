8e0a2d89c6ce6f4f9b98680d8860218d7cce4f8f
Jayce Houtman <jhoutman@houtknots.com>
2023-12-18 20:52:07 +0100

    Recheck on window resize and keep sidebar open on the opened article

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,26 @@
 })()
 
 
+// Change sidebar width on window resize
+$( window ).on( "resize", function() {
+  if(window.innerWidth <= 991) {
+    $('#sidebar').addClass('collapsed').removeClass('opened');
+  } else {
+    $('#sidebar').removeClass('collapsed').removeClass('opened');
+  }
+} );
+
 $(document).ready(function () {
-  // Add 'collapsed' class if window width is less than or equal to 768px
-  if(window.innerWidth <= 768) {
+  // Add 'collapsed' class if window width is less than or equal to 991px
+  if(window.innerWidth <= 991) {
     $('#sidebar').addClass('collapsed');
   }
 
   $('#sidebarToggle').click(function () {
-    // Toggle 'opened' class
-    $('#sidebar').toggleClass('opened');
-
     // If 'collapsed' class exists, remove it and adjust navbar classes
     // Else, add 'collapsed' class and remove 'opened' class, then adjust navbar classes
     if ($('#sidebar').hasClass('collapsed')) {
-      $('#sidebar').removeClass('collapsed');
+      $('#sidebar').removeClass('collapsed').addClass('opened');
       $('#navbar').addClass('navbar-with-sidebar').removeClass('navbar-full');
     } else {
       $('#sidebar').addClass('collapsed').removeClass('opened');
